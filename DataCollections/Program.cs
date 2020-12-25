779a0cc80b04d14e4df9803293520a3821228e5c
Rashmi <poddar.rashmi@gmail.com>
2020-12-25 18:10:00 -0500

    Initial commit

---
+++ DataCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
